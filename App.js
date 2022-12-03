a5fbec79ba85a255d6a9893dac17698936e173d7
Hju Kneyck Flores <hjucode@gmail.com>
2022-12-04 00:17:25 +0800

    Implement image picker on seller registration

--- App.js
+++ App.js
@@ -1,113 +1,186 @@
-import React from "react";
-import {View, Button, Text} from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import SignIn from './screens/signIn';
-import Registration from './screens/registration';
-import Tabs from './navigation/tabs';
-import EditProduct from "./screens/editProduct";
-import AddProduct from "./screens/addProduct";
-import TransactionDetails from "./screens/transactionDetail";
-import Ongoing from "./screens/ongoing";
-import Delivered from "./screens/delivered";
-import SelectionPage from "./screens/selectionpage";
-import CustomerRegistration from "./screens/customerregistration";
-import CustomerSignIn from "./screens/customerSignin";
-import BottomNavigation from "./navigation/bottomNavigation";
-import VegetableDetails from "./screens/VegetableDetails";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import { Button, Text, View } from "react-native";
 
-import Vegetables from './screens/Categories/Vegetables';
-import Fruits from './screens/Categories/Fruits';
-import ProductDetails from './screens/ProductDetails';
-import ProductReviews from './screens/ProductReviews';
-import CheckoutForm from './screens/CheckoutForm';
-import ShippingAddress from './screens/ShippingAddress';
-import EditAddress from './screens/EditAddress';
-import EditCustomerProfile from './screens/EditCustomerProfile';
-import ToPay from './screens/ToPay';
-import ToReceive from './screens/ToReceive';
-import ToReview from './screens/ToReview';
-import WriteReview from './screens/WriteReview';
-import RecentTransactions from './screens/RecentTransactions';
-import OrderReviews from './screens/OrderReviews';
-import SearchScreen from './screens/SearchScreen';
+import BottomNavigation from "./navigation/bottomNavigation";
+import Tabs from "./navigation/tabs";
+import Fruits from "./screens/Categories/Fruits";
+import Vegetables from "./screens/Categories/Vegetables";
+import CheckoutForm from "./screens/CheckoutForm";
+import EditAddress from "./screens/EditAddress";
+import EditCustomerProfile from "./screens/EditCustomerProfile";
+import OrderReviews from "./screens/OrderReviews";
+import ProductDetails from "./screens/ProductDetails";
+import ProductReviews from "./screens/ProductReviews";
+import RecentTransactions from "./screens/RecentTransactions";
+import SearchScreen from "./screens/SearchScreen";
+import ShippingAddress from "./screens/ShippingAddress";
+import ToPay from "./screens/ToPay";
+import ToReceive from "./screens/ToReceive";
+import ToReview from "./screens/ToReview";
+import VegetableDetails from "./screens/VegetableDetails";
+import WriteReview from "./screens/WriteReview";
+import AddProduct from "./screens/addProduct";
+import CustomerSignIn from "./screens/customerSignin";
+import CustomerRegistration from "./screens/customerregistration";
+import Delivered from "./screens/delivered";
+import EditProduct from "./screens/editProduct";
+import Ongoing from "./screens/ongoing";
+import Registration from "./screens/registration";
+import SelectionPage from "./screens/selectionpage";
+import SignIn from "./screens/signIn";
+import TransactionDetails from "./screens/transactionDetail";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+global.backendUrl = "https://stark-earth-17329.herokuapp.com";
+
 const App = () => {
-  return(
+  return (
     <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="SelectionPage" component={SelectionPage} />
+        <Stack.Screen name="SellerRegistration" component={Registration} />
         <Stack.Screen
-          name="SelectionPage"
-          component={ SelectionPage }
+          name="CustomerRegistration"
+          component={CustomerRegistration}
         />
-        <Stack.Screen name="SellerRegistration" component={ Registration } />
-        <Stack.Screen name="CustomerRegistration" component={ CustomerRegistration } />
-        <Stack.Screen name="Tabs" component={ Tabs }  />
-        <Stack.Screen name="SellerSignIn" component={ SignIn } />
-        <Stack.Screen name="CustomerSignIn" component={ CustomerSignIn } />
-          <Stack.Screen name="BottomNavigation" component={ BottomNavigation } options = {{ 
+        <Stack.Screen name="Tabs" component={Tabs} />
+        <Stack.Screen name="SellerSignIn" component={SignIn} />
+        <Stack.Screen name="CustomerSignIn" component={CustomerSignIn} />
+        <Stack.Screen
+          name="BottomNavigation"
+          component={BottomNavigation}
+          options={{
             headerShown: false,
-          }} />
-          <Stack.Screen name="Vegetables" component={ Vegetables } options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="Fruits" component={ Fruits } options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="ProductDetails" component={ ProductDetails } options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="VegetableDetails" component={ VegetableDetails } options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="ProductReviews" component={ ProductReviews } options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="CheckoutForm" component={ CheckoutForm } options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="ShippingAddress" component={ ShippingAddress } options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="EditAddress" component={ EditAddress} options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="EditCustomerProfile" component={ EditCustomerProfile } options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="ToPay" component={ ToPay } options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="ToReceive" component={ ToReceive } options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="ToReview" component={ ToReview } options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="WriteReview" component={ WriteReview } options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="RecentTransactions" component={ RecentTransactions } options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="OrderReviews" component={ OrderReviews } options = {{ 
-            headerShown: false
-          }} />
-          <Stack.Screen name="SearchScreen" component={ SearchScreen } options = {{ 
-            headerShown: false
-          }} />
-        <Stack.Screen name="EditProduct" component={ EditProduct } />
-        <Stack.Screen name="AddProduct" component={ AddProduct } />
-        <Stack.Screen name="TransactionDetails" component={ TransactionDetails } />
-        <Stack.Screen name="Ongoing" component={ Ongoing } />
-        <Stack.Screen name="Delivered" component={ Delivered } />
+          }}
+        />
+        <Stack.Screen
+          name="Vegetables"
+          component={Vegetables}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Fruits"
+          component={Fruits}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="ProductDetails"
+          component={ProductDetails}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="VegetableDetails"
+          component={VegetableDetails}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="ProductReviews"
+          component={ProductReviews}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="CheckoutForm"
+          component={CheckoutForm}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="ShippingAddress"
+          component={ShippingAddress}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="EditAddress"
+          component={EditAddress}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="EditCustomerProfile"
+          component={EditCustomerProfile}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="ToPay"
+          component={ToPay}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="ToReceive"
+          component={ToReceive}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="ToReview"
+          component={ToReview}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="WriteReview"
+          component={WriteReview}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="RecentTransactions"
+          component={RecentTransactions}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="OrderReviews"
+          component={OrderReviews}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="SearchScreen"
+          component={SearchScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name="EditProduct" component={EditProduct} />
+        <Stack.Screen name="AddProduct" component={AddProduct} />
+        <Stack.Screen
+          name="TransactionDetails"
+          component={TransactionDetails}
+        />
+        <Stack.Screen name="Ongoing" component={Ongoing} />
+        <Stack.Screen name="Delivered" component={Delivered} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
 export default App;
--- android/app/src/main/java/com/anonymous/agrikonnectmobile/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/anonymous/agrikonnectmobile/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.agrikonnectmobile.newarchitecture.modules;
+package com.anonymous.agrikonnectmobile.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
